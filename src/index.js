2fd75dbb31352f4a324ba84f9311d533c5ec28c7
Cyrus Bigham <bighamcyrus@gmail.com>
2022-08-13 18:11:42 -0700

    index file contants header tag, 15:30

--- src/index.js
+++ src/index.js
@@ -4,7 +4,27 @@ import App from './App.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const head = ReactDOM.createRoot(document.querySelector('head'));
+
+head.render(
+  <>
+    <meta charset="utf-8" />
+    <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <meta
+      name="description"
+      content="Web site created using create-react-app"
+    />
+    <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
+    
+    <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+    
+    <title>Cyrus Bigham React Portfolio</title>
+    </>
+)
 root.render(
+
   
     <App />
  
